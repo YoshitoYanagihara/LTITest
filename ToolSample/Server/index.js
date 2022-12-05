d0bd65c2405ba90726589832f543dac2f56a7171
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-12-05 22:43:01 +0900

    とりあえずアプリケーション起動APIが実行されるまで

--- ToolSample/Server/index.js
+++ ToolSample/Server/index.js
@@ -5,8 +5,24 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.post("/login", (req, res) => {
+  const loginRequest = {
+    scope: "openid",
+    response_type: "id_token",
+    client_id: "RlxYqlQm7Bv7Uyd",
+    redirect_uri: "http://localhost:3000/app",
+    login_hint: req.body.login_hint,
+    state: "dummyState",
+    nonce: "dummyNonce",
+    response_mode: "form_post",
+    prompt: "none",
+    lti_message_hint: req.body.lti_message_hint,
+  }
+  res.redirect("http://localhost/mod/lti/auth.php?" + new URLSearchParams(loginRequest).toString())
+})
+
+app.post("/app", (req, res) => {
   console.log(req.body)
-  res.send("hoge")
+  res.send(req.body)
 })
 
 app.listen(3000, () => {
