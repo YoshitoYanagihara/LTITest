fa1409efcca918525648274cdda40cc3b1eaae31
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-12-05 22:28:37 +0900

    とりあえずログに吐き出す

--- ToolSample/Server/index.js
+++ ToolSample/Server/index.js
@@ -1,6 +1,14 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post("/login", (req, res) => {
+  console.log(req.body)
+  res.send("hoge")
+})
+
 app.listen(3000, () => {
   console.log("Start Server!")
 })
